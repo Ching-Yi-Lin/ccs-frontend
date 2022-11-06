f0c5e4d564db9cf7dde56cc27d59615fbcc3f689
Carol <lovellcarol@hotmail.com>
2022-11-06 14:38:39 +1300

    Updated image config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const DEFAULT_BACKEND_DOMAIN = '127.0.0.1';
 const DEFAULT_BACKEND_URL = `http://${DEFAULT_BACKEND_DOMAIN}:1337`;
 const DEFAULT_API_URL = `${DEFAULT_BACKEND_URL}/api`;
-const USE_IMAGE_PROVIDER = false;
+const USE_IMAGE_PROVIDER = true;
 
 module.exports = {
   images: {
@@ -15,7 +15,7 @@ module.exports = {
   },
   publicRuntimeConfig: {
     API_URL: process.env.API_URL || DEFAULT_API_URL,
-    BACKEND_URL: process.env.BACKEND_URL || (process.env.NODE_ENV === 'production' || USE_IMAGE_PROVIDER ? '' : DEFAULT_BACKEND_URL),
+    BACKEND_URL: process.env.NODE_ENV === 'production' || USE_IMAGE_PROVIDER ? '' : DEFAULT_BACKEND_URL,
   }
 };
 
